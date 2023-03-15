937c872dfb21c2c94aba43ee58e695deb7aa3c1d
TomatoZero <76494366+TomatoZero@users.noreply.github.com>
2023-03-15 12:00:56 +0200

    Update commit

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,16 +4,14 @@ using System.Threading;
 namespace Lab1{
     internal class Program{
         public static void Main(string[] args) {
-            Console.WriteLine("Number threads:");
-            var n = Convert.ToInt32(Console.ReadLine());
+            int n = 20;
 
             var myThreads = new MyThread[n];
             var threads = new Thread[n];
 
-            int j = n + 1;
-            
+            var rn = new Random();
             for (var i = 0; i < n; i++) {
-                myThreads[i] = new MyThread(i, i + 1, (j--) * 10000);
+                myThreads[i] = new MyThread(i, i + 1, (rn.Next(10, 100)) * 100);
                 threads[i] = new Thread(myThreads[i].Run);
                 threads[i].Start();
             }
